7a0118358dcee6de9ebb855103f5ff8e8d8750f6
Panaghis Mavrokefalos <panaghismavrokefalos@gmail.com>
2022-01-21 19:15:21 +0200

    (WIP) Bug when upstreams have no matches

--- importNew.R
+++ importNew.R
@@ -82,6 +82,11 @@ importNew <- function(filename) {
     #   $UpStream: the index of that upstream.
     #   $DownMates: a list of the downstream indexes which the upstream is
     # matched to.
+    # FIXME
+    # The above assertion is currently not true. Upstreams with no matching
+    # downstreams are not included at all. A possible fix could be the
+    # equivalent of an SQL outer join.
+    # A dataset which triggers this bug is import/round1m2-1.
     mate <- lapply(marketIdxs, function(mIdx) {
         DT[
             Market == mIdx & Match == 1,
