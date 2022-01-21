92eb033bc777a582cdad5894cb1a0db8d2e29e58
Panaghis Mavrokefalos <panaghismavrokefalos@gmail.com>
2022-01-21 18:49:52 +0200

    (WIP) Minor changes in maximizeNew

--- maximizeNew.R
+++ maximizeNew.R
@@ -1,13 +1,6 @@
 maximizeNew <- function(objective, params) {
     set.seed(params$RandomSeed)
-    # Can we "sublist" somehow?
-    control <- list(
-        NP      = params$NP,
-        itermax = params$itermax,
-        trace   = params$trace,
-        reltol  = params$reltol,
-        CR      = params$CR,
-        F       = params$F)
+    control <- params[c("NP", "itermax", "trace", "reltol", "CR", "F")]
     outDEoptim <- DEoptim(objective, params$lower, params$upper, control = control)
     bestmem <-  outDEoptim$optim$bestmem
     bestval <- -outDEoptim$optim$bestval
