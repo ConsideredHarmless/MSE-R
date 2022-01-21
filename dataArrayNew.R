d626898d999c7039c0a7e70ddf3b24d970c5dcab
Panaghis Mavrokefalos <panaghismavrokefalos@gmail.com>
2022-01-21 16:32:05 +0200

    (WIP) Simplify CdataArrayNew

--- dataArrayNew.R
+++ dataArrayNew.R
@@ -9,42 +9,21 @@ CdataArrayNew <- function(distanceMatrices, ineqmembers) {
     mIdxs <- seq_along(ineqmembers)
     f <- function(mIdx) {
         ineqmembersSingle <- ineqmembers[[mIdx]]
-        fctUpIdxsAll <- ineqmembersSingle$fctUpIdxs
-        fctDnIdxsAll <- ineqmembersSingle$fctDnIdxs
-        cfcUpIdxsAll <- ineqmembersSingle$cfcUpIdxs
-        cfcDnIdxsAll <- ineqmembersSingle$cfcDnIdxs
         ineqIdxs <- 1:ineqmembersSingle$numIneqs
+        h <- function(uIdxLHS, dIdxLHS, uIdxRHS, dIdxRHS) {
+            valLHS <- distanceMatrices[[mIdx]][, dIdxLHS, uIdxLHS]
+            valRHS <- distanceMatrices[[mIdx]][, dIdxRHS, uIdxRHS]
+            return(valLHS-valRHS)
+        }
         g <- function(ineqIdx) {
-            fctUpIdxs <- fctUpIdxsAll[[ineqIdx]]
-            fctDnIdxs <- fctDnIdxsAll[[ineqIdx]]
-            cfcUpIdxs <- cfcUpIdxsAll[[ineqIdx]]
-            cfcDnIdxs <- cfcDnIdxsAll[[ineqIdx]]
+            fctUpIdxs <- ineqmembersSingle$fctUpIdxs[[ineqIdx]]
+            fctDnIdxs <- ineqmembersSingle$fctDnIdxs[[ineqIdx]]
+            cfcUpIdxs <- ineqmembersSingle$cfcUpIdxs[[ineqIdx]]
+            cfcDnIdxs <- ineqmembersSingle$cfcDnIdxs[[ineqIdx]]
             termIdxs <- seq_along(fctUpIdxs)
-            h <- function(termIdx) {
-                mIdxLHS <- mIdx
-                mIdxRHS <- mIdx
-                uIdxLHS <- fctUpIdxs[termIdx]
-                dIdxLHS <- fctDnIdxs[termIdx]
-                uIdxRHS <- cfcUpIdxs[termIdx]
-                dIdxRHS <- cfcDnIdxs[termIdx]
-                valLHS <- distanceMatrices[[mIdxLHS]][, dIdxLHS, uIdxLHS]
-                valRHS <- distanceMatrices[[mIdxRHS]][, dIdxRHS, uIdxRHS]
-                return(valLHS-valRHS)
-            }
-            # TODO Since this only needs distanceMatrices, we can move this up
-            # in scope.
-            hNew <- function(uIdxLHS, dIdxLHS, uIdxRHS, dIdxRHS) {
-                valLHS <- distanceMatrices[[mIdx]][, dIdxLHS, uIdxLHS]
-                valRHS <- distanceMatrices[[mIdx]][, dIdxRHS, uIdxRHS]
-                return(valLHS-valRHS)
-            }
-            return(rowSums(mapply(hNew, fctUpIdxs, fctDnIdxs, cfcUpIdxs, cfcDnIdxs)))
-            # TODO is it worth replacing this with
-            #   mapply(..., fctUpIdxs, ...) ?
-            # return(rowSums(sapply(termIdxs, h)))
+            return(rowSums(mapply(h, fctUpIdxs, fctDnIdxs, cfcUpIdxs, cfcDnIdxs)))
         }
         return(sapply(ineqIdxs, g))
     }
-    # https://stackoverflow.com/q/3414078
     return(do.call(cbind, lapply(mIdxs, f)))
 }
