aeecaaa6b762408fa487e241ef2ed73c4e0ffd87
Panaghis Mavrokefalos <panaghismavrokefalos@gmail.com>
2022-01-21 17:13:37 +0200

    (WIP) Add docs to CdataArrayNew

--- dataArrayNew.R
+++ dataArrayNew.R
@@ -1,10 +1,20 @@
-# TODO docs
-# old:
-#  dataArray[[ineqIdx]]
-#  dataArray[[ineqIdx]][paramIdx]
-# new:
-#  dataArray[, ineqIdx]
-#  dataArray[paramIdx, ineqIdx]
+# CdataArrayNew(distanceMatrices, ineqmembers) computes the data array. For the
+# data array, see section 5.2 in [1]. For the distanceMatrices structure, see
+# the importNew function. For the ineqmembers structure, see the CineqmembersNew
+# function.
+#
+# Returns an array of dimension (noAttr, sumNumIneqs), where sumNumIneqs is the
+# sum over all markets of the number of inequalities of each market.
+#
+# For old code, replace
+#    dataArray[[ineqIdx]]
+#    dataArray[[ineqIdx]][paramIdx]
+# by
+#    dataArray[, ineqIdx]
+#    dataArray[paramIdx, ineqIdx]
+# respectively.
+#
+# [1] David Santiago and Fox, Jeremy (2009). "A Toolkit for Matching Maximum Score Estimation and Point and Set Identified Subsampling Inference".
 CdataArrayNew <- function(distanceMatrices, ineqmembers) {
     mIdxs <- seq_along(ineqmembers)
     f <- function(mIdx) {
