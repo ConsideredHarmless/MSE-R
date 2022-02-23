374d969a90016587eefab540d97e4b5c61b8ba23
Panaghis Mavrokefalos <panaghismavrokefalos@gmail.com>
2022-02-23 20:48:33 +0200

    Fix bug in generateAssignmentMatrix

--- matchingNew.R
+++ matchingNew.R
@@ -1,31 +1,85 @@
+# TODO move to Roxygen documentation and fix LaTeX
+#
 # generateAssignmentMatrix(payoffMatrix, quotaU, quotaD) calculates the optimal
 # matching for a given market by solving the binary ILP:
-#       max     \sum_{i=1}^{N_U} \sum_{j=1}^{N_D} P_{i,j}*X_{i,j}
+#       max     \sum_{i=1}^{N_D} \sum_{j=1}^{N_U} P_{i,j}*X_{i,j}
 #     X \in S
-#       s.t.    \sum_{j=1}^{N_D} X_{i,j} <= Q_{U,i}, 1 <= i <= N_U
-#               \sum_{i=1}^{N_U} X_{i,j} <= Q_{D,j}, 1 <= j <= N_D
+#       s.t.    \sum_{j=1}^{N_U} X_{i,j} <= q_{D,i}, 1 <= i <= N_D
+#               \sum_{i=1}^{N_D} X_{i,j} <= q_{U,j}, 1 <= j <= N_U
 # where
 #   N_U and N_D are the numbers of upstreams and downstreams for this market.
-#   S = {0,1}^{N_U \times N_D}.
-#   X_{i,j} is equal to 1 if upstream i and downstream j match, and 0 otherwise.
+#   S = {0,1}^{N_D \times N_U}.
+#   X_{i,j} is equal to 1 if upstream j and downstream i match, and 0 otherwise.
 #   P_{i,j} is the value of the payoff function for the matching above.
-#   Q_U and Q_D are vectors of quotas for the upstreams and downstreams;
-#   upstream i can have no more than Q_{U,i} matches total, and similarly for
-#   Q_{D,j}.
+#   q_U and q_D are vectors of quotas for the upstreams and downstreams;
+#   upstream j can have no more than q_{U,j} matches total, and similarly for
+#   q_{D,i}.
 # By flattening the two dimensions of X and P, we arrive at a binary ILP
 # formulation.
 #
-# For payoffMatrix, see the payoffNew.R file.
-# quotaU and quotaD must be vectors of length noU and noD respectively,
-# containing non-negative integers. Alternatively, either can be a non-negative
-# integer. In this case, the quota vector is this value, repeated the
-# appropriate times.
+# Note that we follow the convention used in the R code, and the first dimension
+# of the matrices indexes the downstreams. We use i to index downstreams, and j
+# to index upstreams.
 #
-# Returns an array of dimension (noD, noU). Its element indexed by [dIdx, uIdx]
-# is 1 if that downstream-upstream pair matches, and 0 otherwise.
+# Define now x = vec(X) and p = vec(P), where vec flattens the matrices into
+# vectors of length N_D*N_U by concatenating columns. Observe that the two
+# inequalities can be written as:
+# \sum_{j=1}^{N_U} X_{i,j} <= q_{D,i} <=>
+# \sum_{j=1}^{N_U} X_{i,j} 1_{N_U \times 1}_j <= q_{D,i} <=>
+# (X 1_{N_U \times 1})_i <= q_{D,i}
+# and
+# \sum_{i=1}^{N_D} X_{i,j} <= q_{U,j} <=>
+# \sum_{i=1}^{N_D} X^T_{j,i} 1_{N_D \times 1}_i <= q_{U,j} <=>
+# (X^T 1_{N_D \times 1})_j <= q_{U,j} <=>
+# where 1_{m \times n} is the m \times n matrix containing all ones.
+#
+# The inequalities can now be written as:
+# X 1_{N_U \times 1} <= q_D
+# X^T 1_{N_D \times 1} <= q_U
+# where <= is interpreted element-wise.
+#
+# The identity connecting the matrix products in the two vector spaces is:
+# vec(A B C) = (C^T \otimes A) vec(B)
+#
+# Since for a vector x we have x = vec(x) = vec(x^T), we have:
+# I_{N_D} X 1_{N_U \times 1} <= q_D <=>
+# vec(I_{N_D} X 1_{N_U \times 1}) <= vec(q_D) <=>
+# (1_{1 \times N_U} \otimes I_{N_D}) x <= q_D
+# and
+# I_{N_U} X^T 1_{N_D \times 1} <= q_U <=>
+# vec(1_{1 \times N_D} X I_{N_U}) <= vec(q_U^T) <=>
+# (I_{N_U} \otimes 1_{1 \times N_D}) x <= q_U
+#
+# We can now write the vectorized formulation of the ILP:
+#   max     p^T x
+#    x
+#   s.t.    M_D x <= q_D
+#           M_U x <= q_U
+#
+# where M_D and M_U are defined above.
+
+#' Generate assignment matrix
+#'
+#' Finds the optimal matching in a market for the given payoffs and quotas.
+#'
+#' @param payoffMatrix An array of payoffs for all upstream-downstream pairs in
+#' this market. Note that the first dimension indexes the downstreams. See the
+#' payoffNew.R file for more details.
+#' @param quotaU A vector of length \code{numU}, containing integers in the
+#' range \code{0:numD}. Its \code{j}-th element is the maximum allowed number of
+#' matches for the corresponding upstream. Alternatively, it can be a single
+#' non-negative integer. In this case, the quota vector is this value, repeated
+#' the appropriate number of times.
+#' @param quotaD As \code{quotaU}, but for downstreams.
+#'
+#' @return An array of dimension \code{(numD, numU)}. Its element indexed by \code{[dIdx, uIdx]}
+#' is \code{1} if that downstream-upstream pair matches, and \code{0} otherwise.
+#' @export
+#'
+#' @examples
 generateAssignmentMatrix <- function(payoffMatrix, quotaU, quotaD) {
-    numU <- dim(payoffMatrix)[1]
-    numD <- dim(payoffMatrix)[2]
+    numD <- dim(payoffMatrix)[1]
+    numU <- dim(payoffMatrix)[2]
     if (length(quotaU) == 1) {
         quotaU <- rep(quotaU, numU)
     }
@@ -40,39 +94,27 @@ generateAssignmentMatrix <- function(payoffMatrix, quotaU, quotaD) {
     }
 
     # Constraints
-    # Let x = vec(X^T)[1] the flattened variables. We will convert the
-    # constraints to the form M*x <= b, where M is a matrix, b is a vector, and
-    # the inequality is entrywise.
+    # Let x = vec(X) the flattened variables. We will convert the constraints to
+    # the form M*x <= b, where M is a matrix, b is a vector, and the inequality
+    # is element-wise.
     # We construct M and b from three blocks:
-    # m1 and m2 correspond to the constraints above. m3 constrains the values of
-    # the variables in the set {0,1}.
-    # [1] The transposition is because R uses column-major order. The
-    # correspondence is:
-    #   X_{i,j} == x_{numD*(i-1)+j}.
-
-    # m1 has the form
-    # [1 1 ... 1                        ]
-    # [          1 1 ... 1              ]
-    # [                    ...          ]
-    # [                        1 1 ... 1]
-    # where omitted values are 0, the blocks of 1s have length numD, and there
-    # are numU rows.
-    m1 <- matrix(
-        as.vector(outer(cbind(rep(1, numD)), data.matrix(diag(numU)))),
-        nrow = numU, ncol = numD*numU, byrow=TRUE)
-    # m2 has the form
-    # [I_numU I_numU ... I_numU]
-    # where I_n is the n x n identity matrix, and there are numD such blocks.
-    m2 <- matrix(outer(diag(numD), rep(1, numU)), nrow = numD, ncol = numD*numU)
-    m3 <- diag(numU*numD)
-    m <- rbind(m1, m2, m3)
-    b <- c(quotaU, quotaD, rep(1, numU*numD))
+    # M_D and M_U correspond to the constraints above. M_S constrains the values
+    # of the variables in the set {0,1}.
+    onesD = t(matrix(rep(1, numD)))
+    onesU = t(matrix(rep(1, numU)))
+    idD = diag(numD)
+    idU = diag(numU)
+    mD <- kronecker(onesU, idD)
+    mU <- kronecker(idU, onesD)
+    mS <- diag(numD*numU)
+    m <- rbind(mD, mU, mS)
+    b <- c(quotaD, quotaU, rep(1, numD*numU))
 
     # Reminder that payoffMatrix is indexed as [dIdx, uIdx]. No transposition is
     # needed here.
     f.obj <- as.vector(payoffMatrix)
     f.con <- m
-    f.dir <- rep("<=", numU + numD + numU*numD)
+    f.dir <- rep("<=", numD + numU + numD*numU)
     f.rhs <- b
     result <- lp("max", f.obj, f.con, f.dir, f.rhs)
     x <- result$solution
