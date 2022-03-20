a8cf32c957c753327f436c97b677e3382636f4a1
Panaghis Mavrokefalos <panaghismavrokefalos@gmail.com>
2022-03-20 14:47:05 +0200

    Add "closer look" section to vignettes

--- inst/doc/matched.R
+++ inst/doc/matched.R
@@ -61,3 +61,88 @@ plotCR(cr$estimates)
 ## -----------------------------------------------------------------------------
 cr$samples
 
+## -----------------------------------------------------------------------------
+matchedData$noM
+matchedData$noU
+matchedData$noD
+matchedData$noAttr
+
+## -----------------------------------------------------------------------------
+matchedData$header
+
+## -----------------------------------------------------------------------------
+length(matchedData$distanceMatrices)
+length(matchedData$matchMatrices)
+
+## -----------------------------------------------------------------------------
+mIdx <- 3
+uIdx <- 4
+dIdx <- 7
+matchedData$matchMatrices[[mIdx]][dIdx, uIdx]
+
+## -----------------------------------------------------------------------------
+# Leaving an index blank takes all elements for that dimension.
+matchedData$matchMatrices[[mIdx]][, uIdx]
+# `which` returns the indices where the elements above are `1` -- i.e. the
+# downstream matches of upstream '4'.
+which(matchedData$matchMatrices[[mIdx]][, uIdx] == 1)
+
+## -----------------------------------------------------------------------------
+aIdx <- 3
+matchedData$distanceMatrices[[mIdx]][aIdx, dIdx, uIdx]
+matchedData$distanceMatrices[[mIdx]][, dIdx, uIdx]
+
+## -----------------------------------------------------------------------------
+mIdx <- 1
+length(ineqmembers)
+names(ineqmembers[[mIdx]])
+
+## -----------------------------------------------------------------------------
+ineqmembers[[mIdx]]$numIneqs
+length(ineqmembers[[mIdx]]$fctUpIdxs)
+
+## -----------------------------------------------------------------------------
+mIdx <- 3
+uIdx <- 4
+dIdx <- 7
+ineqIdx <- 1
+termIdx <- 1
+ineqmembers[[mIdx]]$fctUpIdxs[[ineqIdx]][termIdx]
+ineqmembers[[mIdx]]$fctDnIdxs[[ineqIdx]][termIdx]
+ineqmembers[[mIdx]]$cfcUpIdxs[[ineqIdx]][termIdx]
+ineqmembers[[mIdx]]$cfcDnIdxs[[ineqIdx]][termIdx]
+
+## -----------------------------------------------------------------------------
+getIneqTermsIdxs(ineqmembers[[mIdx]], ineqIdx)
+getAllIneqTermsIdxs(ineqmembers[[mIdx]])[[ineqIdx]]
+
+## -----------------------------------------------------------------------------
+dim(dataArray)
+
+## -----------------------------------------------------------------------------
+groupIDs
+
+## -----------------------------------------------------------------------------
+mIdx <- 2
+qualifiedIndices <- which(groupIDs == mIdx)
+qualifiedIndices
+dataArray[, qualifiedIndices]
+
+## -----------------------------------------------------------------------------
+selectedGroups <- c(1, 3)
+qualifiedIndices <- which(groupIDs %in% selectedGroups)
+qualifiedIndices
+
+## -----------------------------------------------------------------------------
+makeBounds(matchedData$noAttr, 100)
+
+## -----------------------------------------------------------------------------
+makeBounds(matchedData$noAttr, 10, -20)
+
+## -----------------------------------------------------------------------------
+bounds <- list(lower=c(-1, -2, -3, -5), upper=c(2, 3, 5, 8))
+bounds
+
+## -----------------------------------------------------------------------------
+optimParams
+
--- inst/doc/unmatched.R
+++ inst/doc/unmatched.R
@@ -12,12 +12,12 @@ filenameUp <- system.file("extdata", "unmatched_testdata_up.csv", package = "max
 filenameDn <- system.file("extdata", "unmatched_testdata_dn.csv", package = "maxscoreest")
 
 ## -----------------------------------------------------------------------------
-dataUnmatched <- importUnmatched(filenameUp, filenameDn)
+unmatchedData <- importUnmatched(filenameUp, filenameDn)
 
 ## -----------------------------------------------------------------------------
 unevalPayoffMatrices <- makeDistanceMatrices(
-    dataUnmatched$attributeMatricesUp,
-    dataUnmatched$attributeMatricesDn)
+    unmatchedData$attributeMatricesUp,
+    unmatchedData$attributeMatricesDn)
 
 ## -----------------------------------------------------------------------------
 beta <- c(0.5, 1.5)
@@ -25,6 +25,59 @@ payoffMatrices <- evaluatePayoffMatrices(unevalPayoffMatrices, beta)
 
 ## -----------------------------------------------------------------------------
 matchMatrices <- CmatchMatrices(
-    payoffMatrices, dataUnmatched$quotasUp, dataUnmatched$quotasDn)
+    payoffMatrices, unmatchedData$quotasUp, unmatchedData$quotasDn)
 matchMatrices
+
+## -----------------------------------------------------------------------------
+unmatchedData$noM
+unmatchedData$noU
+unmatchedData$noD
+unmatchedData$noAttr
+
+## -----------------------------------------------------------------------------
+unmatchedData$headerUp
+unmatchedData$headerDn
+
+## -----------------------------------------------------------------------------
+length(unmatchedData$attributeMatricesUp)
+length(unmatchedData$attributeMatricesDn)
+length(unmatchedData$quotasUp)
+length(unmatchedData$quotasDn)
+
+## -----------------------------------------------------------------------------
+mIdx <- 2
+uIdx <- 1
+aIdx <- 3
+unmatchedData$attributeMatricesUp[[mIdx]][aIdx, uIdx]
+unmatchedData$attributeMatricesUp[[mIdx]][, uIdx]
+
+## -----------------------------------------------------------------------------
+mIdx <- 1
+unmatchedData$quotasUp[[mIdx]]
+unmatchedData$quotasDn[[mIdx]]
+
+## -----------------------------------------------------------------------------
+length(payoffMatrices)
+mIdx <- 1
+dim(payoffMatrices[[mIdx]])
+payoffMatrices[[mIdx]]
+uIdx <- 1
+dIdx <- 2
+payoffMatrices[[mIdx]][dIdx, uIdx]
+
+## -----------------------------------------------------------------------------
+matchMatrices
+
+## -----------------------------------------------------------------------------
+mIdx <- 2
+uIdx <- 2
+dIdx <- 3
+matchMatrices[[mIdx]][dIdx, uIdx]
+
+## -----------------------------------------------------------------------------
+# Leaving an index blank takes all elements for that dimension.
+matchMatrices[[mIdx]][, uIdx]
+# `which` returns the indices where the elements above are `1` -- i.e. the
+# downstream matches of upstream '2'.
+which(matchMatrices[[mIdx]][, uIdx] == 1)
 
